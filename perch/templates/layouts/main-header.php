c2ee5e71ac2dc45efa40d10cab8b7b49c7df68a9
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-07-22 12:46:30 +0100

    Temporarily hiding nav

--- perch/templates/layouts/main-header.php
+++ perch/templates/layouts/main-header.php
@@ -60,9 +60,9 @@
 	<body class="<?php perch_page_attribute('bodyClass', array('template' => 'bits.html' )); ?>">
 		<div itemscope itemtype="http://schema.org/LocalBusiness" class="site-wrapper <?php perch_page_attribute('wrapperClass', array('template' => 'bits.html'));?>">
 			<header class="main-header">
-				<div class="main-nav-wrapper">
+				<nav class="main-nav">
 					<?php perch_pages_navigation(array(
 							'hide-extensions' => true,
 					)); ?>
-				</div>
+				</nav>
 			</header>
