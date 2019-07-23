6176f1aa2544a2201af24bfc96607e5e9c1b659d
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-07-23 18:28:51 +0100

    Parallax tweaks

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <?php perch_layout('main-header'); ?>
 <main class="main-content homepage-main-content">
 		<?php perch_content('Homepage Hero Image'); ?>
+		<?php perch_content("Featured Galleries"); ?>
 		<?php perch_content("Homepage Quotation"); ?>
 		<?php perch_content("Homepage Image collage"); ?>
 </main>
