e1ac85276c62ee405cc9675e00ced9aebc0c91e6
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-07-26 16:27:04 +0100

    Album template and featured album styles

--- perch/templates/layouts/featured-galleries.php
+++ perch/templates/layouts/featured-galleries.php
@@ -2,5 +2,13 @@
   <h2 class="featured-galleries-section-heading">
     Featured Galleries
   </h2>
-
+    <?php
+      perch_gallery_albums(array(
+        'template' => 'homepage_featured_albums.html',
+        'image' => true,
+        'filter' => 'featured',
+        'match' => 'eq',
+        'value' => true,
+        'count'=>3
+      )); ?>
 </section>
