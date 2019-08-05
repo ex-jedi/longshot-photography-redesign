66676bbc2edd7d26f4f832f383274b849a3ecf04
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-08-05 19:19:01 +0100

    Simple header and submark template

--- about-me.php
+++ about-me.php
@@ -1,5 +1,5 @@
 <?php include('perch/runtime.php'); ?>
-<?php perch_layout('main-header'); ?>
+<?php perch_layout('simple-header'); ?>
 <main class="main-content about-me-main-content" id="main-content">
 
 </main>
