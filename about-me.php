921d5cb1d51d4838a197f164c2dcd1a6df133359
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-08-05 20:17:36 +0100

    Adding contact form to about me page

--- about-me.php
+++ about-me.php
@@ -1,7 +1,8 @@
 <?php include('perch/runtime.php'); ?>
 <?php perch_layout('simple-header'); ?>
 <main class="main-content about-me-main-content" id="main-content">
-
+  <?php perch_content("About Me Section"); ?>
+  <?php perch_content("Contact Form"); ?>
 </main>
 <?php perch_layout('main-footer'); ?>
 <!-- TODO: Remove debug -->
