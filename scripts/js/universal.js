80a0b19f3e39542e57ae3c34200c1028cb4495b9
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-08-12 18:56:22 +0100

    Service image bg box shadow

--- scripts/js/universal.js
+++ scripts/js/universal.js
@@ -29,12 +29,3 @@ document.addEventListener('keyup', evt => {
 // =========================================
 
 let rellax = new Rellax('.rellax');
-
-const serviceImage = document.querySelectorAll('.service-companion-image');
-console.log('img', serviceImage);
-
-for (var i = 0; i < serviceImage.length; i++) {
-  console.log(serviceImage.length);
-  serviceImage[0].src = '/images/image-companion-one.png';
-  serviceImage[1].src = '/images/image-companion-two.png';
-}
