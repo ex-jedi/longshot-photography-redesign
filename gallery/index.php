4904662d92b5086076f1b25c22fbc96274c2eb9f
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-08-13 20:35:05 +0100

    Gallery index album initial layout

--- gallery/index.php
+++ gallery/index.php
@@ -1,16 +1,18 @@
 <?php include('../perch/runtime.php'); ?>
 <?php perch_layout('simple-header'); ?>
 <main id="main-content" class="main-content gallery-main-content">
-		<section class="gallery-section">
-			<h2 class="section-heading">
+		<section class="gallery-index-section">
+			<h2 class="section-heading gallery-section-heading">
 				Galleries
 			</h2>
-		<?php
-			perch_gallery_albums(array(
-					'template'=>'gallery_index_page.html',
-					'image'=>true
-			));
-		?>
+		<div class="gallery-index-wrapper">
+			<?php
+				perch_gallery_albums(array(
+						'template'=>'gallery_index_page.html',
+						'image'=>true
+				));
+				?>
+		</div>
 	</section>
 	<?php perch_content("General Contact Form"); ?>
 </main>
