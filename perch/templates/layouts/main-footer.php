26d156373f1aee8d292cc7538ca63e9181df7195
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-08-16 19:31:11 +0100

    Homepage featured images now lazy loading

--- perch/templates/layouts/main-footer.php
+++ perch/templates/layouts/main-footer.php
@@ -25,6 +25,7 @@
 <script type="text/javascript" src="/scripts/minified/rellax.min.js" ></script>
 <!-- Lazy Load -->
 <script src="/scripts/minified/lazyload.min.js"></script>
+<!-- General JS file -->
 <script type="text/javascript" src="/scripts/minified/universal-min.js"></script><!-- Local universal JS files -->
 </body>
 </html>
