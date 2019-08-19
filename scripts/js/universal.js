cd0283358460b7491948a9c88460b946e22674af
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-08-19 16:57:24 +0100

    Fade up in with data offset attribute

--- scripts/js/universal.js
+++ scripts/js/universal.js
@@ -47,14 +47,25 @@ const scroll =
 
 const elementsToShow = document.querySelectorAll('.fade-up-in');
 
+// Set how far the element is from the top of the viewport
+let offsetTop = document
+  .querySelector('.fade-up-in')
+  .getAttribute('data-offset-top');
+
+// Set how far the element 'peeks' into the viewport
+let offsetBottom = document
+  .querySelector('.fade-up-in')
+  .getAttribute('data-offset-bottom');
+
+// If the offsett data values aren't set, default to 0
+if (!offsetTop) offsetTop = 0;
+if (!offsetBottom) offsetBottom = 0;
+console.log(offsetBottom);
+
 // Helper function to check when element is in viewport, adapted from: http://stackoverflow.com/a/7557433/274826
 
 function isElementInViewport(el) {
   const bounding = el.getBoundingClientRect();
-
-  //Set how far the element 'peeks' into the viewport
-  let offsetTop = 50; // Offset in pixels or bounding.height * 0.9 (fraction of elements height)
-  let offsetBottom = 0; // Offset in pixels or bounding.height * 0.9 (fraction of elements height)
 
   let offsetFirst = offsetTop - bounding.height;
   let offsetSecond = bounding.height - offsetBottom;
@@ -91,7 +102,7 @@ let rellax = new Rellax('.rellax');
 // =========================================
 // ** Lazy load  **
 // =========================================
-var lazyLoadInstance = new LazyLoad({
+let lazyLoadInstance = new LazyLoad({
   elements_selector: '.lazy',
   // ... more custom settings?
 });
