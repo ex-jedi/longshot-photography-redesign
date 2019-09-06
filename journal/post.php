81d57fd6f25ad076c112c9a605d822643a3f7511
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-09-06 16:57:18 +0100

    Simplifying post category template

--- journal/post.php
+++ journal/post.php
@@ -3,13 +3,14 @@
 	<main class="blog-main-content blog-post-main-content">
 		<article class="blog-post-article" itemscope itemtype="https://schema.org/BlogPosting" itemprop="blogPost">
 			<section class="blog-section blog-post-section">
-			<?php perch_blog_post(perch_get('s')); ?>
-			<?php perch_blog_post_categories(perch_get('s')); ?>
-			<?php perch_blog_post_tags(perch_get('s')); ?>
+				<?php perch_blog_post_categories(perch_get('s')); ?>
+				<?php perch_blog_post(perch_get('s')); ?>
 			</section>
 			<section>
-			<?php perch_content("General Contact Form"); ?>
+				<?php perch_content("General Contact Form"); ?>
 			</section>
 		</article>
 	</main>
 <?php perch_layout('main-footer'); ?>
+
+<?php PerchUtil::output_debug(); ?>
