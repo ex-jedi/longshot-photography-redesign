472ae48351d6a2e83da5ae200f57a2bc7184305e
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-09-06 13:16:35 +0100

    Adding blog post section

--- journal/post.php
+++ journal/post.php
@@ -2,14 +2,14 @@
 <?php perch_layout('blog-post-header'); ?>
 	<main class="blog-main-content blog-post-main-content">
 		<article class="blog-post-article" itemscope itemtype="https://schema.org/BlogPosting" itemprop="blogPost">
+			<section class="blog-section blog-post-section blog-post-meta">
 			<?php perch_blog_post(perch_get('s')); ?>
-			<section class="blog-section blog-post-meta">
-				<?php perch_blog_post_categories(perch_get('s')); ?>
-				<?php perch_blog_post_tags(perch_get('s')); ?>
+			<?php perch_blog_post_categories(perch_get('s')); ?>
+			<?php perch_blog_post_tags(perch_get('s')); ?>
+			</section>
+			<section>
+			<?php perch_content("General Contact Form"); ?>
 			</section>
 		</article>
-		<section>
-			<?php perch_content("General Contact Form"); ?>
-		</section>
 	</main>
 <?php perch_layout('main-footer'); ?>
