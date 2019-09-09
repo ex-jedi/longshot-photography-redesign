fb943c9e16ceff2d9d30d68caddabf5dcd3749f9
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-09-09 14:33:44 +0100

    Blog index page title

--- journal/index.php
+++ journal/index.php
@@ -2,6 +2,7 @@
 <?php perch_layout('blog-post-header'); ?>
   <main class="blog-main-content blog-index-main-content">
   <section class="blog-section blog-index-section" >
+    <?php perch_content("Blog Index Page Title"); ?>
     <?php perch_blog_recent_posts(5); ?>
   </section>
   <section class="contact-form section">
