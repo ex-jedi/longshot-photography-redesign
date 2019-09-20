2db526da9ca16e318d420d94a61e8b7b80e1cf3f
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-09-20 13:20:30 +0100

    Working archive page

--- journal/archive.php
+++ journal/archive.php
@@ -1,7 +1,7 @@
 <?php include('../perch/runtime.php'); ?>
 <?php perch_layout('simple-header'); ?>
 <main class="blog-main-content archive-main-content">
-	<section class="blog-section blog-archive-section">
+	<section class="blog-section blog-index-section blog-archive-section">
 			<?php
 		        // defaults for all modes
 		        $posts_per_page = 10;
@@ -14,9 +14,13 @@
 
 		        /* --------------------------- POSTS BY CATEGORY --------------------------- */
 		        if (perch_get('cat')) {
-		            echo '<h1 class="archive-section-heading section-heading">'.perch_blog_category(perch_get('cat'), true).'</h1>';
+		            echo '<h1 class="blog-index-page-title section-heading">'.perch_blog_category(perch_get('cat'), true).'</h1>';
 
-
+						echo '<div class="blog-index-category-list">';
+								$categories = perch_blog_categories(array(
+									'template' => 'category_link.html'
+								));
+						echo '</div>';
 
 							echo '<div class="index-featured-posts">';
 								perch_blog_custom(array(
@@ -50,15 +54,35 @@
 		      		// No other options have been used; no posts have been displayed yet.
 		      		// So display all posts.
 
-		      		echo '<h1 class="archive-section-heading section-heading">Archive</h1>';
+							echo '<h1 class="blog-index-page-title section-heading">Archive</h1>';
 
-		      		perch_blog_custom(array(
-							'template'   => $template,
-							'count'      => $posts_per_page,
-							'sort'       => $sort_by,
-							'sort-order' => $sort_order,
-		                    ));
-		      	}
+							echo '<div class="blog-index-category-list">';
+							$categories = perch_blog_categories(array(
+								'template' => 'category_link.html'
+							));
+
+							echo '</div>';
+
+								echo '<div class="index-featured-posts">';
+									perch_blog_custom(array(
+										'count'      => 2,
+										'template'   => $template,
+										'sort'       => $sort_by,
+										'sort-order' => $sort_order,
+									));
+							echo '</div>';
+							echo '<div class="index-non-featured-posts">';
+							perch_blog_custom(array(
+										'template' => $template,
+										'sort'       => $sort_by,
+										'sort-order' => $sort_order,
+										'start' => 3,
+										'count' => 20,
+										'paginate' => false,
+									));
+
+							echo '</div>';
+								}
 
 		    ?>
 	</section>
