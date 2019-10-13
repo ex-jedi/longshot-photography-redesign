7017a8bf6ae1a66dbcf6aae06dbd78cb4d04321e
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-10-13 13:14:49 +0100

    Reordering js and linting

--- scripts/js/universal.js
+++ scripts/js/universal.js
@@ -33,47 +33,25 @@ document.addEventListener('keyup', evt => {
 // Detect request animation frame
 const scroll =
   window.requestAnimationFrame ||
-  window.webkitRequestAnimationFrame ||
-  window.mozRequestAnimationFrame ||
-  window.msRequestAnimationFrame ||
-  window.oRequestAnimationFrame ||
-  // IE Fallback, you can even fallback to onscroll
+  // IE Fallback
   // eslint-disable-next-line func-names
   function(callback) {
     window.setTimeout(callback, 1000 / 60);
   };
-
-// Target which elements will have reveal class added
-
 const elementsToShow = document.querySelectorAll('.fade-up-in');
 
-// Set how far the element is from the top of the viewport
-let offsetTop = document
-  .querySelector('.fade-up-in')
-  .getAttribute('data-offset-top');
-
-// Set how far the element 'peeks' into the viewport
-let offsetBottom = document
-  .querySelector('.fade-up-in')
-  .getAttribute('data-offset-bottom');
-
-// If the offsett data values aren't set, default to 0
-if (!offsetTop) offsetTop = 0;
-if (!offsetBottom) offsetBottom = 0;
-console.log(offsetBottom);
-
-// Helper function to check when element is in viewport, adapted from: http://stackoverflow.com/a/7557433/274826
-
+// Helper function from: http://stackoverflow.com/a/7557433/274826
 function isElementInViewport(el) {
-  const bounding = el.getBoundingClientRect();
-
-  let offsetFirst = offsetTop - bounding.height;
-  let offsetSecond = bounding.height - offsetBottom;
+  const rect = el.getBoundingClientRect();
   return (
-    bounding.top >= offsetFirst &&
-    bounding.bottom <=
-      (window.innerHeight + offsetSecond ||
-        document.documentElement.clientHeight + offsetSecond)
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <=
+        (window.innerHeight || document.documentElement.clientHeight)) ||
+    (rect.top >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight))
   );
 }
 
