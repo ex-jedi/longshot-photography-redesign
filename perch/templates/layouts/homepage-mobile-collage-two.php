c76d67563e10158dde835b1d38baf53ba70e6192
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-11-09 16:35:44 +0000

    Home page mobile colage image two

---
+++ perch/templates/layouts/homepage-mobile-collage-two.php
@@ -0,0 +1,7 @@
+  <div class="homepage-mobile-collage-image-wrapper full-width-collage-image-wrapper">
+    <img class="homepage-mobile-collage-image full-width-collage lazy" data-src="<perch:content id="collageMobileOne" type="image" width="1000" label="Collage Mobile Image One" help="For mobile devices" divider-before="Mobile Images">" alt="<perch:content id="altMobileOne" type="text" label="Image description">"
+    data-sizes="100vh"
+    data-srcset="<perch:content type="image" id="collageMobileOne" width="1000" > 1000w,
+                <perch:content type="image" id="collageMobileOne" width="750" > 750w,
+                <perch:content type="image" id="collageMobileOne" width="500" > 500w">
+  </div>
