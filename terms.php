85447537e9925e776a3eaec362f32f3d582474a5
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-11-10 11:43:26 +0000

    Initial legals layout and taller footer

--- terms.php
+++ terms.php
@@ -1,7 +1,12 @@
 <?php include('perch/runtime.php'); ?> <!-- Perch initialisation code -->
 <?php perch_layout('simple-header'); ?>
 	<div class="page-wrapper">
-		<main class="legals terms-and conditions">
-        <?php perch_content('Terms And Conditions'); ?>
-		</main>
+		<section class="legals-section">
+			<h2 class="section-heading services-page-heading">
+				Terms Of Use
+			</h2>
+			<main class="legals terms-and conditions">
+					<?php perch_content('Terms And Conditions'); ?>
+			</main>
+		</section>
 		<?php perch_layout('main-footer'); ?>
