f8a94ead29658d68f9450b81eba35897e1c8da2e
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-11-11 21:12:04 +0000

    404 page

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php include('perch/runtime.php'); ?> <!-- Perch initialisation code -->
+	<?php perch_layout('simple-header'); ?>
+		<main class="404">
+			<section class="legals-section">
+				<h2 class="section-heading legals-page-heading">
+					404
+				</h2>
+					<?php perch_content('404'); ?>
+			</section>
+			</main>
+	<?php perch_layout('main-footer'); ?>
