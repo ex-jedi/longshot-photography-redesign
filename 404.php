88e9be6ae159e961386f4314e496b71b8f7f2b16
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-11-12 09:23:31 +0000

    404 Sass page

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php include('perch/runtime.php'); ?> <!-- Perch initialisation code -->
 	<?php perch_layout('simple-header'); ?>
-		<main class="404">
-			<section class="404-section">
+		<main class="four-oh-four-main-content" id="main">
+			<section class="four-oh-four-section">
 			 <?php perch_content("404"); ?>
 			</section>
 		</main>
