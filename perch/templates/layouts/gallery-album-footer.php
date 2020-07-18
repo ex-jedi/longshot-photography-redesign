4e08afe8fca8dc3e3f10832dcb42be9db35de1fb
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-07-18 21:49:40 +0100

    Positioned buttons

--- perch/templates/layouts/gallery-album-footer.php
+++ perch/templates/layouts/gallery-album-footer.php
@@ -35,10 +35,6 @@
 <script>
   const elem = document.querySelector('.main-carousel');
   const flkty = new Flickity(elem, {
-    // options
-    cellAlign: 'center',
-    contain: true,
-    wrapAround: true,
   });
 </script>
 
