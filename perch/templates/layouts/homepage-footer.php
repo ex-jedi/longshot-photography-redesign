c2a01e23a0fffa84744137ed30e4c587889a993c
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-07-25 18:36:54 +0100

    Image collage fade in animation

---
+++ perch/templates/layouts/homepage-footer.php
@@ -0,0 +1,35 @@
+<footer class="main-footer colour-will-change">
+	<div class="footer-social-media">
+		<a href="https://www.instagram.com/longshot_photo" class="footer-social-media-link">
+			<img class="footer-social-media-icon" src="/images/instagram-icon-for-dark-background.svg">
+		</a>
+		<a href="https://twitter.com/longshot_photo" class="footer-social-media-link">
+			<img class="footer-social-media-icon" src="/images/twitter-icon-for-dark-background.svg">
+		</a>
+	</div>
+	<div class="footer-legal">
+		<p class="copyright" >&copy; <?php echo date("Y"); ?> KEN LONG</p>
+		<p class="privacy"><a href="/privacy-and-cookie-policy">PRIVACY &amp; COOKIE POLICY</a></p>
+		<p class="terms"><a href="/terms">TERMS OF USE</a></p>
+	</div>
+	<div class="footer-promo">
+		<p>
+			WEB DESIGN & DEVELOPMENT, BRAND DESIGN & ILLUSTRATION BY <a href="https://phoenixandphoenix.co/" class="promo-link">phoenix & phoenix</a>
+		</p>
+	</div>
+</footer>
+</div> <!-- wrapper div -->
+<!-- TODO: Change Vue to production  version -->
+<script type="text/javascript" src="/scripts/minified/vue-min.js" ></script>
+<!-- Rellax -->
+<script type="text/javascript" src="/scripts/minified/rellax.min.js" ></script>
+<!-- Lazy Load -->
+<script src="/scripts/minified/lazyload.min.js"></script>
+<!-- ScrollMagic -->
+<script src="/scripts/minified/ScrollMagic.min.js"></script>
+<!-- General JS file -->
+<script type="text/javascript" src="/scripts/minified/universal-min.js"></script>
+<!-- Homepage JS  -->
+<script src="/scripts/minified/homepage-min.js"></script>
+</body>
+</html>
