005f970dacaee5843e53d886351d748b57ccfb45
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-07-27 12:38:03 +0100

    Homepage mobile collage image animation

--- scripts/js/homepage.js
+++ scripts/js/homepage.js
@@ -2,6 +2,8 @@
 // ** Picture animation  **
 // *=========================================
 
+// ********** Main Collage Animation **********
+
 const collageImage = document.querySelectorAll('.homepage-collage-image');
 const collageWrapper = document.querySelector('.homepage-collage-section');
 
@@ -13,6 +15,24 @@ collageImage.forEach(item => {
     reverse: false,
   })
     .setClassToggle(collageImage, 'collage-image-reveal')
-    // TODO: Remove indicators
+    .addTo(controller);
+});
+
+// ********** Mobile Collage Animation **********
+
+const mobileCollageImage = document.querySelectorAll(
+  '.homepage-mobile-collage-image',
+);
+
+console.log(mobileCollageImage);
+
+mobileCollageImage.forEach(item => {
+  const sceneTwo = new ScrollMagic.Scene({
+    triggerElement: item,
+    triggerHook: 0.8,
+    reverse: false,
+  })
+    .setClassToggle(item, 'mobile-collage-image-reveal')
+    .addIndicators()
     .addTo(controller);
 });
