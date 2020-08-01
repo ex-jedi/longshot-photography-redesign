e8a46e084f2ce1f30a5eb30b8ea6f37cad124d23
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-08-01 15:18:34 +0100

    Destroy Flickity for smaller screens

--- scripts/js/gallery-album.js
+++ scripts/js/gallery-album.js
@@ -1,18 +1,28 @@
-const carousel = document.querySelector('.main-carousel');
-const flkty = new Flickity(carousel, {
-  imagesLoaded: true,
-  percentPosition: false,
-});
-
-const imgs = carousel.querySelectorAll('.gallery-album-image');
-// get transform property
-const docStyle = document.documentElement.style;
-const transformProp = typeof docStyle.transform === 'string' ? 'transform' : 'WebkitTransform';
-
-flkty.on('scroll', function() {
-  flkty.slides.forEach(function(slide, i) {
-    const img = imgs[i];
-    const x = ((slide.target + flkty.x) * -1) / 3;
-    img.style[transformProp] = `translateX(${x}px)`;
+const sliderQuery = window.matchMedia('(max-width: 1100px)');
+function sliderHide() {
+  const carousel = document.querySelector('.main-carousel');
+  const flkty = new Flickity(carousel, {
+    imagesLoaded: true,
+    percentPosition: false,
   });
-});
+
+  const imgs = carousel.querySelectorAll('.gallery-album-image');
+  // get transform property
+  const docStyle = document.documentElement.style;
+  const transformProp = typeof docStyle.transform === 'string' ? 'transform' : 'WebkitTransform';
+
+  flkty.on('scroll', function() {
+    flkty.slides.forEach(function(slide, i) {
+      const img = imgs[i];
+      const x = ((slide.target + flkty.x) * -1) / 3;
+      img.style[transformProp] = `translateX(${x}px)`;
+    });
+  });
+
+  if (sliderQuery.matches) {
+    flkty.destroy();
+  }
+}
+sliderHide();
+
+window.addEventListener('resize', sliderHide);
